9612ff7dd0d02e581e022f319a9bdba72db0e259
LeonesHanter <61425569+LeonesHanter@users.noreply.github.com>
2025-12-26 10:17:24 +0400

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from yaml import safe_load
 from typing import List
+import os
 
 class ChatConfig(BaseModel):
     chat_id: int
@@ -18,4 +19,6 @@ class BotConfig(BaseModel):
 def load_config(path: str = "config.yaml") -> BotConfig:
     with open(path, 'r', encoding='utf-8') as f:
         data = safe_load(f)
+    # Заменяем 'ваш_токен' на токен из .env
+    data['token'] = os.getenv("VK_ACCESS_TOKEN", data.get('token'))
     return BotConfig(**data)
